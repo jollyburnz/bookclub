6cf46c65dc652dfa0808b0eb9d27f6573ded4f8f
jollyburnz <j@cksonl.in>
2025-02-04 02:10:57 -0500

    added a distDir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-  }
-  
-  module.exports = nextConfig
+    distDir: '.next',
+}
+
+module.exports = nextConfig
